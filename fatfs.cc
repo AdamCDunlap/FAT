f0cfd1c9e214406d76f50a68621eec94a04d5852
Adam Dunlap <dunlapadam@gmail.com>
2016-02-26 00:09:56 -0800

    Things mostly work

--- fatfs.cc
+++ fatfs.cc
@@ -17,6 +17,9 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <cassert>
+#include <iterator>
+#include <memory>
 
 using std::string;
 using std::cerr;
@@ -29,15 +32,16 @@ static const size_t cluster_size = 4*1024;
 string adam_file_contents("Hello there\n");
 static std::fstream filestore;
 
-using fat_entry = uint32_t;
-static const size_t fat_entry_size = sizeof(fat_entry);
+using cluster_idx_t = uint32_t;
+using filesize_t = uint32_t;
+static const size_t cluster_idx_size = sizeof(cluster_idx_t);
 static const size_t fat_entries = 1 +   (filestore_size - cluster_size - 1)
-                                   / (cluster_size + fat_entry_size);
-static const size_t fat_size = fat_entries * fat_entry_size;
+                                   / (cluster_size + cluster_idx_size);
+static const size_t fat_size = fat_entries * cluster_idx_size;
 static const size_t fat_clusters = 1 + (fat_size - 1) / cluster_size;
 static const size_t padded_fat_size = fat_clusters * cluster_size;
-static const size_t padded_fat_entries = padded_fat_size / fat_entry_size;
-static const size_t fat_entires_per_cluster = cluster_size / fat_entry_size;
+static const size_t padded_fat_entries = padded_fat_size / cluster_idx_size;
+static const size_t fat_entires_per_cluster = cluster_size / cluster_idx_size;
 
 static const size_t superblock_cluster = 0;
 static const size_t fat_cluster = 1;
@@ -45,11 +49,18 @@ static const size_t fat_cluster = 1;
 static const size_t data_start_block = fat_cluster + fat_clusters;
 
 static const char directory_delim = '/';
+static const size_t directory_size = cluster_size;
+
+static const cluster_idx_t end_of_file_cluster_marker
+    = std::numeric_limits<cluster_idx_t>::max();
 
 // Writes 1 cluster to the filestore from the pointer
 template<typename T>
 void write_cluster(size_t cluster_num, const T* data) {
   cout << "Writing to cluster " << cluster_num << endl;
+  if (cluster_num == superblock_cluster) {
+    cout << "[32;1mWARNING: Writing to superblock cluster![0;0m" << endl;
+  }
 
   filestore.seekp(cluster_num * cluster_size);
   filestore.write(reinterpret_cast<const char*>(data), cluster_size);
@@ -65,7 +76,7 @@ void read_cluster(size_t cluster_num, T* data) {
 }
 
 class FAT_t {
-  std::vector<fat_entry> FAT;
+  std::vector<cluster_idx_t> FAT;
  public:
 
   FAT_t() : FAT(padded_fat_entries)
@@ -73,12 +84,18 @@ class FAT_t {
   }
 
   // Sets a value in the in-memory FAT. DOES NOT persist
-  void weak_set(size_t idx, fat_entry val) {
-    FAT[idx] = val;
+  void weak_set(size_t idx, cluster_idx_t val) {
+    assert(idx >= data_start_block);
+    cout << "Writing " << val << " to FAT at index " << idx << endl;
+    if (idx == val) {
+      cout << "WARNING: Making loop in FAT" << endl;
+      exit(3);
+    }
+    FAT[idx-data_start_block] = val;
   }
 
   // Sets a value in the FAT and persists the change to disk
-  void set(size_t idx, fat_entry val) {
+  void set(size_t idx, cluster_idx_t val) {
     weak_set(idx, val);
 
     const size_t cluster_idx = idx / cluster_size;
@@ -87,8 +104,8 @@ class FAT_t {
   }
 
   // Gets a value from the in-memory FAT
-  fat_entry get(size_t idx) {
-    return FAT[idx];
+  cluster_idx_t get(size_t idx) {
+    return FAT[idx-data_start_block];
   }
   
   // Loads the FAT from the filestore file
@@ -115,8 +132,8 @@ class FAT_t {
 
 static const size_t directory_entry_size = 32;
 struct directory_entry {
-  uint32_t size{0};
-  uint32_t starting_cluster{0};
+  filesize_t size{0};
+  cluster_idx_t starting_cluster{0};
   struct {
     // 0 if file, 1 if directory
     //unsigned char filetype : 1;
@@ -127,14 +144,84 @@ struct directory_entry {
   std::array<char, max_name_len> name{{'\0'}};
 };
 
-using directory = std::array<
-  directory_entry, cluster_size / directory_entry_size>;
+static const size_t files_in_dir_cluster = cluster_size / directory_entry_size;
+using directory = std::array<directory_entry, files_in_dir_cluster>;
 
 struct superblock {
-  // other stuff I guess
-  static const size_t superblock_pad_amt = cluster_size - sizeof(directory_entry);
+  // other stuff I guess?
+  static const size_t superblock_pad_amt
+    = cluster_size - sizeof(directory_entry);
   std::array<char, superblock_pad_amt> padding = {{0}};
   directory_entry root_directory_entry;
+};
+
+// A directory can iterate through its directory entries
+class directory_c {
+  cluster_idx_t start_cluster;
+ public:
+  class iterator
+      : public std::iterator<std::forward_iterator_tag, directory_entry> {
+
+    friend class directory_c;
+    using dirent_array = std::array<directory_entry, files_in_dir_cluster>;
+    std::shared_ptr<dirent_array> data;
+
+    size_t dir_idx = 0;
+    cluster_idx_t cur_cluster;
+
+    iterator(cluster_idx_t start_cluster)
+        : data(std::make_shared<dirent_array>()) {
+      cur_cluster = start_cluster;
+      read_cluster(cur_cluster, data->data());
+    }
+
+   public:
+    iterator() = default;
+    iterator(const iterator&) = default;
+    iterator& operator=(const iterator&) = default;
+
+    directory_entry operator*() const {
+      return (*data)[dir_idx];
+    }
+    iterator& operator++() {
+      if (dir_idx == files_in_dir_cluster) {
+        cur_cluster = FAT.get(cur_cluster);
+        dir_idx = 0;
+        if (cur_cluster == end_of_file_cluster_marker) {
+          // The iterator's at the end, no point in keeping the old directory
+          // around
+          data.reset();
+        } else {
+          if (!data.unique()) {
+            // Reset the pointer to new empty space
+            data = std::make_shared<dirent_array>();
+          }
+          read_cluster(cur_cluster, data->data());
+        }
+      } else {
+        ++dir_idx;
+      }
+      return *this;
+    }
+    // not writing operator++(int) because it's stupid (would require
+    // allocation and deallocation every time it went over a cluster boundary).
+    // If someone wants to use the old value they can just use std::next()
+    bool operator==(const iterator& other) {
+      return dir_idx == other.dir_idx && cur_cluster == other.cur_cluster;
+    }
+    bool operator!=(const iterator& other) {
+      return !(*this == other);
+    }
+  };
+
+  iterator begin() {
+    return iterator(start_cluster);
+  }
+
+  iterator end() {
+    return iterator(end_of_file_cluster_marker);
+  }
+
 };
 
 bool break_off_last_path_entry(const string& path,
@@ -161,6 +248,7 @@ bool get_directory_entry_from_path(const string& path, directory_entry& ret) {
     superblock s;
     read_cluster(superblock_cluster, &s);
     ret = s.root_directory_entry;
+    cout << "Root direcotry is at cluster " << ret.starting_cluster << endl;
     return true;
   }
 
@@ -178,6 +266,7 @@ bool get_directory_entry_from_path(const string& path, directory_entry& ret) {
   }
   if (!parent_directory_entry.flags.filetype) {
     cerr << "get_directory_entry_from_path called with nondirectory" << endl;
+    return false;
   }
 
 
@@ -188,29 +277,36 @@ bool get_directory_entry_from_path(const string& path, directory_entry& ret) {
     return true;
   }
 
+  cluster_idx_t parent_dir_cluster = parent_directory_entry.starting_cluster;
   // Get parent's directory from their directory entry
-  directory parent_directory;
-  cout << "Reading parent directory" << endl;
-  read_cluster(parent_directory_entry.starting_cluster, &parent_directory);
+  do {
+    cout << "get_directory_entry_from_path reading from parent cluster " << parent_dir_cluster << endl;
+    directory parent_directory;
+    read_cluster(parent_dir_cluster, &parent_directory);
 
-  for (const directory_entry& d : parent_directory) {
-    if (child_name == d.name.data()) {
-      cout << "Found a match! " << child_name << " == " << d.name.data() << endl;
-      ret = d;
-      return true;
+    for (const directory_entry& d : parent_directory) {
+      if (child_name == d.name.data()) {
+        cout << "Found a match! " << child_name << " == " << d.name.data() << endl;
+        ret = d;
+        return true;
+      }
     }
-  }
+    cout << "getattr couldn't find child; moving from cluster " << parent_dir_cluster;
+    parent_dir_cluster = FAT.get(parent_dir_cluster);
+    cout << " to cluster " << parent_dir_cluster << endl;
+  } while(parent_dir_cluster != end_of_file_cluster_marker);
   cerr << "get_directory_from_path called with nonexistant child" << endl;
   return false;
 }
 
-static size_t find_next_free_cluster() {
-  for (size_t i=0; i<fat_entries; ++i) {
+static cluster_idx_t find_next_free_cluster() {
+  for (size_t i=data_start_block; i<fat_entries+data_start_block; ++i) {
     if (FAT.get(i) == 0) {
-      return i+data_start_block;
+      return i;
     }
   }
-  return -1;
+  cerr << "Can't find any free clusters!" << endl;
+  return end_of_file_cluster_marker;
 }
 
 static int adam_getattr(const char *cpath, struct stat *stbuf)
@@ -221,12 +317,9 @@ static int adam_getattr(const char *cpath, struct stat *stbuf)
 
   memset(stbuf, 0, sizeof(struct stat));
 
-  //stbuf->st_dev = 2; // TODO
-  //stbuf->st_ino = 3; // TODO
-
   directory_entry dir_ent;
   if (!get_directory_entry_from_path(path, dir_ent)) {
-    cerr << "getattr called on nonexistant file :c" << endl;
+    cerr << "getattr called on nonexistant file " << path << endl;
     return -ENOENT;
   }
 
@@ -244,7 +337,6 @@ static int adam_getattr(const char *cpath, struct stat *stbuf)
   //stbuf->st_gid = getgid();
   //stbuf->st_rdev = 0;
 
-  //stbuf->st_blksize = 512;
   //stbuf->st_blocks = 3;
   //struct timespec time0 = {1455428262, 0};
   //stbuf->st_atim = time0;
@@ -275,9 +367,10 @@ static int adam_readlink(const char *path, char *buf, size_t size)
 
 
 static int adam_readdir(const char *cpath, void *buf, fuse_fill_dir_t filler,
-                       off_t offset, struct fuse_file_info *fi)
+                       off_t signed_offset, struct fuse_file_info *fi)
 {
   string path(cpath);
+  size_t offset = signed_offset; // why is it given to us signed? :/
   cerr << "ADAM: adam_readdir called on ``" << path << "'' with offset " << offset << endl;
 
   directory_entry dir_ent;
@@ -287,21 +380,35 @@ static int adam_readdir(const char *cpath, void *buf, fuse_fill_dir_t filler,
   }
   if (!dir_ent.flags.filetype) {
     cout << "readdir called on file..." << endl;
-    return -ENOENT;
+    return -ENOTDIR;
   }
 
-  directory dir;
-  read_cluster(dir_ent.starting_cluster, &dir);
+  size_t absolute_offset = offset;
+  cluster_idx_t dir_cluster = dir_ent.starting_cluster;
+  // Get parent's directory from their directory entry
+  do {
+    directory dir;
+    read_cluster(dir_cluster, &dir);
 
-  for (size_t entry_num = offset; entry_num < dir.size(); ++entry_num) {
-    const directory_entry& d = dir[entry_num];
-    if (d.name[0] == '\0') {
-      break;
+    for (size_t entry_num = offset;
+         entry_num < dir.size(); ++entry_num, ++absolute_offset) {
+      const directory_entry& d = dir[entry_num];
+
+      if (d.name[0] == '\0') {
+        break;
+      }
+
+      if (filler(buf, d.name.data(), nullptr, absolute_offset+1) != 0) {
+        return 0;
+      }
     }
-    if (filler(buf, d.name.data(), nullptr, entry_num+1) != 0) {
-      return 0;
+    dir_cluster = FAT.get(dir_cluster);
+    if (offset >= files_in_dir_cluster) {
+      offset -= files_in_dir_cluster;
+    } else {
+      offset = 0;
     }
-  }
+  } while(dir_cluster != end_of_file_cluster_marker);
   return 0;
 }
 
@@ -311,21 +418,40 @@ static int adam_mknod(const char *path, mode_t mode, dev_t rdev)
   return -ENOSYS;
 }
 
-static void mkdir_at_directory_entry(
+// Makes a directory, puts info in given directory entry
+static int mkdir_at_directory_entry(
     directory_entry& d, const string& name) {
 
   strncpy(d.name.data(), name.c_str(), d.name.size()-1);
   d.name[d.name.size()-1] = '\0';
-  d.size = cluster_size;
+  d.size = directory_size;
   d.starting_cluster = find_next_free_cluster();
+  if (d.starting_cluster == end_of_file_cluster_marker) {
+    cout << "Couldn't find a free cluster" << endl;
+    return -ENOSPC;
+  }
   d.flags.filetype = 1;
 
-  FAT.set(d.starting_cluster-data_start_block, -1);
+  FAT.set(d.starting_cluster, end_of_file_cluster_marker);
 
+  // Make the new directory and put . and .. into it
   directory dir;
+  //directory_entry dot_dir_ent, dot_dot_dir_ent;
+
+  //dot_dir_ent = d;
+  //dot_dir_ent.name = {"."};
+
+  //dot_dot_dir_ent = parent_dir_ent;
+  //dot_dot_dir_ent.name = {".."};
+
+  //dir[0] = dot_dir_ent;
+  //dir[1] = dot_dot_dir_ent;
+
   write_cluster(d.starting_cluster, &dir);
 
   cout << "Made new directory, put it at cluster " << d.starting_cluster << endl;
+
+  return 0;
 }
 
 static int adam_mkdir(const char *cpath, mode_t mode)
@@ -335,41 +461,95 @@ static int adam_mkdir(const char *cpath, mode_t mode)
   cout << "mkdir called on path: " << path << endl;
 
   string parent_path, child_name;
-  if (!break_off_last_path_entry(path, parent_path, child_name)) {
-    cerr << "mkdir called on invalid path" << endl;
-    return false;
-  }
 
   if (child_name.length() >= directory_entry::max_name_len) {
+    cout << "mkdir: filename too long" << endl;
+    return -ENAMETOOLONG;
+  }
+
+  if (!break_off_last_path_entry(path, parent_path, child_name)) {
+    cerr << "mkdir called on invalid path" << endl;
     return -ENOENT;
   }
 
   directory_entry parent_dir_ent;
   if (!get_directory_entry_from_path(parent_path, parent_dir_ent)) {
     cout << "mkdir: parent path doesn't exist" << endl;
-    return -1;
+    return -ENOENT;
   }
 
   cout << "Mkdir directory entry lookup succeeded" << endl;
 
-  directory parent_dir;
-  read_cluster(parent_dir_ent.starting_cluster, &parent_dir);
 
-  for (size_t dir_entry_num = 0;
-       dir_entry_num < parent_dir.size();
-       ++dir_entry_num) {
-    directory_entry& d = parent_dir[dir_entry_num];
-    if (d.name[0] == '\0') {
-      mkdir_at_directory_entry(d, child_name);
-      write_cluster(parent_dir_ent.starting_cluster, &parent_dir);
-      cout << "Mkdir done" << endl;
-      return 0;
+
+  cluster_idx_t parent_dir_cluster = parent_dir_ent.starting_cluster;
+  cluster_idx_t last_parent_dir_cluster;
+  // Get parent's directory from their directory entry
+  do {
+    do {
+      directory parent_directory;
+      read_cluster(parent_dir_cluster, &parent_directory);
+
+      for (directory_entry& d : parent_directory) {
+
+        if (d.name[0] == '\0') {
+          int err = mkdir_at_directory_entry(d, child_name);
+          if (err) {
+            cout << "mkdir: mkdir_at_directory_entry failed, returning " << err << endl;
+            return err;
+          }
+          write_cluster(parent_dir_cluster, &parent_directory);
+          cout << "Mkdir done" << endl;
+          return 0;
+        }
+      }
+      last_parent_dir_cluster = parent_dir_cluster;
+      parent_dir_cluster = FAT.get(parent_dir_cluster);
+    } while(parent_dir_cluster != end_of_file_cluster_marker);
+
+    // TODO: this should be abstracted somewhat
+
+    cout << "Parent directory too full to make new directory, "
+            "so we'll make it bigger! " << endl;
+
+    cluster_idx_t newparentcluster = find_next_free_cluster();
+    if (newparentcluster == end_of_file_cluster_marker) {
+      cout << "Filesystem too full to make new directory" << endl;
+      return -ENOSPC;
     }
-  }
+    cout << "Putting new cluster at " << newparentcluster << endl;
+    FAT.set(last_parent_dir_cluster, newparentcluster);
+    FAT.set(newparentcluster, end_of_file_cluster_marker);
+    parent_dir_cluster = newparentcluster;
 
-  cout << "Parent directory too full to make new directory" << endl;
+    parent_dir_ent.size += directory_size;
 
-  return -1;
+    directory parent_dir_extended;
+    write_cluster(parent_dir_cluster, &parent_dir_extended);
+    cout << "Done making new cluster" << endl;
+  } while(true);
+
+
+
+
+
+  //directory parent_dir;
+  //read_cluster(parent_dir_ent.starting_cluster, &parent_dir);
+
+  //for (size_t dir_entry_num = 0;
+  //     dir_entry_num < parent_dir.size();
+  //     ++dir_entry_num) {
+  //  directory_entry& d = parent_dir[dir_entry_num];
+  //  if (d.name[0] == '\0') {
+  //    int err = mkdir_at_directory_entry(d, child_name, parent_dir_ent);
+  //    if (err) {
+  //      return err;
+  //    }
+  //    write_cluster(parent_dir_ent.starting_cluster, &parent_dir);
+  //    cout << "Mkdir done" << endl;
+  //    return 0;
+  //  }
+  //}
 }
 
 static int adam_unlink(const char *path)
@@ -533,7 +713,7 @@ int main(int argc, char *argv[])
 {
   static_assert(filestore_size > 0, "No room to store backend");
   static_assert(cluster_size > 0, "0 size cluster");
-  static_assert(padded_fat_entries * fat_entry_size == padded_fat_size,
+  static_assert(padded_fat_entries * cluster_idx_size == padded_fat_size,
       "cluster size not multiple of fat entry size");
   static_assert(sizeof(directory) == cluster_size,
       "Size of directory is not cluster size");
@@ -541,7 +721,7 @@ int main(int argc, char *argv[])
       "directory entry too big to store root directory entry in superblock");
 
   if (argc < 1) {
-    cerr << "First argument should be name of file" << endl;
+    cerr << "First argument should be name of backing file" << endl;
     return 2;
   }
   string filestorename(argv[1]);
@@ -560,7 +740,11 @@ int main(int argc, char *argv[])
     FAT.reset();
 
     superblock sup;
-    mkdir_at_directory_entry(sup.root_directory_entry, "/");
+    int err = mkdir_at_directory_entry(sup.root_directory_entry, "/");
+    if (err) {
+      cerr << "Error creating root directory " << err << endl;
+      return -1;
+    }
     write_cluster(superblock_cluster, &sup);
 
     filestore.close();
@@ -594,5 +778,5 @@ int main(int argc, char *argv[])
   adam_oper.flag_nullpath_ok = 0;
 
   umask(0);
-  return fuse_main(argc, argv, &adam_oper, NULL);
+  return fuse_main(argc, argv, &adam_oper, nullptr);
 }
